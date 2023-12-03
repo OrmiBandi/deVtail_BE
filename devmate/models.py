0c45e2497be57ca7077522d7bce6866e4b77a511
nayeongdev <nayeongkim.dev@gmail.com>
2023-12-04 01:46:55 +0900

    :sparkles: Feat: DevMate, UserBlock, UserReport 모델링
    - 수정 파일
    devmate/models.py
    
    - 추가한 기능
    DevMate, UserBlock, UserReport 모델 추가

--- devmate/models.py
+++ devmate/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class DevMate:
+    '''
+    DevMate 모델
+    '''
+
+    send_user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='send_users'
+    )
+    receive_user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='receive_users'
+    )
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class UserBlock:
+    '''
+    사용자 차단 모델
+    '''
+
+    blocking_user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blocking_users'
+    )
+    blocked_user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blocked_users'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = '사용자 차단'
+        verbose_name_plural = '사용자 차단'
+
+
+class UserReport:
+    '''
+    사용자 신고 모델
+    '''
+
+    reporting_user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reporting_users'
+    )
+    reported_user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reported_users'
+    )
+    reason = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = '사용자 신고'
+        verbose_name_plural = '사용자 신고'
