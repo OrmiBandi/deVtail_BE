4386139027d1042a9ca3c0e991595b7c4ee3487f
hantang820 <hantang820@gmail.com>
2023-12-04 14:17:22 +0900

    ✨ Feat: Alert, Schedule, Todo 모델링 -수정 파일 alert/models.py schedule/models.py todo/models.py
    -추가한 기능
    Alert, Schedule, Todo 모델 추가

--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Schedule(models.Model):
+    '''
+    일정 모델
+    '''
+    user = models.ForeignKey(
+        'accounts.User', on_delete=models.CASCADE, related_name='schedules'
+    )
+    schedule_title = models.CharField(max_length=100)
+    schedule_content =models.CharField(max_length=200, null=True, blank=True)
+    schedule_start = models.DateTimeField(null=True)
+    schedule_end = models.DateTimeField(null=True)
+
+    class Meta:
+        verbose_name = '일정'
+        verbose_name_plural = '일정'
