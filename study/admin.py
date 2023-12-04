b60dcba5823bb282e96d981c5e35cae58d17069f
jaemin.kim <jmkim648@gmail.com>
2023-12-04 16:34:25 +0900

    :sparkles: Feat: study > admin.py
    - 수정한 파일
    study > admin.py
    
    - 수정한 내용
    관리자 페이지에 생성한 모델들 등록

--- study/admin.py
+++ study/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Study, Category, Tag, RefLink, Comment, StudyMember
 
-# Register your models here.
+admin.site.register(Study)
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(RefLink)
+admin.site.register(Comment)
+admin.site.register(StudyMember)
