efe04a727be9ff2f38a68055e991e2d6a1fae50a
jaemin.kim <jmkim648@gmail.com>
2023-12-04 14:39:11 +0900

    :recycle: Refactor: Refactor variable code
    - 수정한 파일
    study > models.py
    
    - 수정한 내용
    link_name > content : 링크 이름이 아니라 내용이라서 변수 자체를 변경
    link_category > category : Link 모델 안의 속성이니 link_라고 명시할 필요가 없어보입니다
    
    - 비고
    주석 추가

--- study/models.py
+++ study/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class Study(models.Model):
+    '''
+    스터디 모델
+    '''
     difficulty_choices = [
         ('상', '상'),
         ('중', '중'),
@@ -19,7 +22,7 @@ class Study(models.Model):
     category = models.ForeignKey('Category', on_delete=models.PROTECT)
     leader = models.ForeignKey('accounts.User', on_delete=models.PROTECT)
     goal = models.CharField(max_length=100)
-    thumbnail = models.ImageField(null=True, blank=True)
+    thumbnail = models.ImageField(upload_to='study/imgs/%Y/%m/%d/', null=True, blank=True)
     startdate = models.DateField()
     enddate = models.DateField()
     cycle = models.IntegerField()
@@ -37,28 +40,47 @@ class Study(models.Model):
 
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=20)
+    '''
+    카테고리 모델
+     - 스터디의 카테고리를 설정
+     - 운영진이 카테고리를 생성, 저장하고 목록을 제공하는 방식
+    '''
+    name = models.CharField(max_length=20)
 
     def __str__(self):
-        return self.category_name
+        return self.name
     
 
 class Tag(models.Model):
+    '''
+    태그 모델
+     - 스터디의 태그 설정
+     - 스터디 생성 시 사용자가 입력한 태그를 저장
+    '''
     study = models.ForeignKey('Study', on_delete=models.CASCADE)
-    tag_name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.tag_name
+        return self.name
 
 class RefLink(models.Model):
+    '''
+    참조링크 모델
+     - 스터디의 참조 링크 저장
+     - category의 경우 GitHub, Notion과 같은 링크 타입을 설명
+    '''
     study = models.ForeignKey('Study', on_delete=models.CASCADE)
-    link_category = models.CharField(max_length=100)
-    link_name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    content = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.link_name
+        return self.content
 
 class Comment(models.Model):
+    '''
+    댓글 모델
+     - 스터디 모집글, 내용 등에 댓글 노출
+    '''
     study = models.ForeignKey('Study', on_delete=models.CASCADE)
     user = models.ForeignKey('accounts.User', on_delete=models.SET_NULL, null=True)
     content = models.CharField(max_length=500)
@@ -71,6 +93,9 @@ class Comment(models.Model):
         return f'스터디 : {self.study}, 작성자 : {self.user.username}'
 
 class StudyMember(models.Model):
+    '''
+    스터디 멤버 모델
+    '''
     study = models.ForeignKey('Study', on_delete=models.CASCADE)
     user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
     is_accepted = models.BooleanField(default=False)
