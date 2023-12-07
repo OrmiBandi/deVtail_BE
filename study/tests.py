020d789c01ae9dd6eb3395e903aa0518116427ef
byeongheon.hwang <elwl5515@gmail.com>
2023-12-08 00:43:36 +0900

    :sparkles: Feat: Study 목록 기능
    - 추가 파일
    .\study\factories.py
    .\study\serializers.py
    
    - 수정 파일
    .\study\tests.py
    .\study\urls.py
    .\study\views.py
    
    - 개발 사항
    목업 데이터를 이용한 스터디 리스트 테스트 기능 구현
    
    - 비고
    테스트 명령어: python manage.py test study.tests.TestStudy.test_study_list

--- study/tests.py
+++ study/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from rest_framework.test import APIClient
+
+from .factories import StudyFactory
+
+
+class TestStudy(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_study_list(self):
+        '''
+        스터디 리스트 기능 테스트
+        '''
+        print('스터디 리스트 기능 테스트 BEGIN')
+        StudyFactory.create_batch(6)
+        response = self.client.get(
+            '/study/',
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 6)
+        print('스터디 리스트 기능 테스트 END')
--- study/views.py
+++ study/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Study
+from .serializers import StudySerializer
+
+class StudyViewSet(ModelViewSet):
+    '''
+    스터디 관련 API
+    '''
+    queryset = Study.objects.all()
+    serializer_class = StudySerializer
+
+    def list(self, request, *args, **kwargs):
+        '''
+        스터디 리스트 조회 API
+        '''
+        return super().list(request, *args, **kwargs)
